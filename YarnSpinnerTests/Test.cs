2ba1d050429fd972255c877651c2f6b1461e96dc
Jonathon Manning <jon@secretlab.com.au>
2015-10-11 10:02:19 +1100

    Started work on tests

--- YarnSpinnerTests/Test.cs
+++ YarnSpinnerTests/Test.cs
@@ -6,10 +6,25 @@ namespace YarnSpinnerTests
 	[TestFixture ()]
 	public class Test
 	{
+
+
+
+		[SetUp()]
+		public void Init()
+		{
+			var newWorkingDir = 
+				System.IO.Path.Combine (Environment.CurrentDirectory, "../../Tests/");
+			Environment.CurrentDirectory = newWorkingDir;
+		}
+
 		[Test ()]
 		public void TestCase ()
 		{
+
+			var d = new Yarn.Dialogue()
+
 		}
 	}
+
 }
 
