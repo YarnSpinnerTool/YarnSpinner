f26534705d6f75641574fbc7e38f78425c287645
Jonathon Manning <jon@secretlab.com.au>
2016-02-14 02:01:04 +1100

    run-tests.sh now exits with 1 if any test scripts fail

--- run-tests.sh
+++ run-tests.sh
@@ -3,7 +3,16 @@
 
 TESTCASES_DIR="Tests/TestCases"
 
+returncode=0
+
 for f in $TESTCASES_DIR/*.node; do
 	echo "Testing $f"
 	./parser.sh $f
+	
+	if [[ $? -ne 0 ]]; then
+		echo "*** ERROR ***"
+		returncode=1
+	fi
 done
+
+exit $returncode
