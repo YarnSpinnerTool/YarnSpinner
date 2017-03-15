8e225422720415b9c7086b1fb18d07e1b34c17ad
Peter Lawler <relwalretep@gmail.com>
2017-03-15 12:32:56 +1100

    Add flavour to the build script to test for build platform, cleaning and installing

--- build.sh
+++ build.sh
@@ -1,15 +1,33 @@
 #!/usr/bin/env bash
 
-xbuild /verbosity:quiet /p:Configuration=Release YarnSpinner.sln
-
-OUTPUT_DLL="YarnSpinner.dll"
-
-BUILD_DIR="YarnSpinner/bin/Release/"
-UNITY_DIR="Unity/Assets/Yarn Spinner/Code/"
-
-if [ -f "$BUILD_DIR/$OUTPUT_DLL" ]; then
-	cp "$BUILD_DIR/$OUTPUT_DLL" "$UNITY_DIR/$OUTPUT_DLL"
-else
-	echo "Build failed."
-	exit 1
+CONFIGURATION="Release"
+VERBOSITY="quiet"
+XBUILD_ARGS="/verbosity:${VERBOSITY} /p:Configuration=${CONFIGURATION}"
+if [ "${OSTYPE}" = "linux-gnu" ]; then
+    XBUILD_ARGS="${XBUILD_ARGS} /p:TargetFrameworkVersion=v4.5"
 fi
+
+if [ -f YarnSpinner/bin/Release/YarnSpinner.dll ]; then
+    xbuild "${XBUILD_ARGS}" /target:clean YarnSpinner.sln
+fi
+
+if [ $? -ne 0 ]; then
+    echo "Error during: xbuild ${XBUILD_ARGS}"
+    exit 1
+fi
+
+# this is an appalling test for not windows or osx and with unity
+if [ "${OSTYPE}" != "linux-gnu" ]; then
+    OUTPUT_DLL="YarnSpinner.dll"
+    BUILD_DIR="YarnSpinner/bin/Release/"
+    UNITY_DIR="Unity/Assets/Yarn Spinner/Code/"
+
+    if [ -f "$BUILD_DIR/$OUTPUT_DLL" ]; then
+        cp -v "$BUILD_DIR/$OUTPUT_DLL" "$UNITY_DIR/$OUTPUT_DLL"
+    else
+        echo "Install for Unity failed."
+        exit 1
+    fi
+fi
+
+
