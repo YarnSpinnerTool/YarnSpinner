c44f093e011e83ec1fe8a6e81ade5975479e0f25
Peter Lawler <relwalretep@gmail.com>
2017-03-20 14:31:40 +1100

    getopts all the build options

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # The MIT License (MIT)
 # 
-# Copyright (c) 2015 Secret Lab Pty. Ltd. and Yarn Spinner contributors.
+# Copyright (c) 2015-2017 Secret Lab Pty. Ltd. and Yarn Spinner contributors.
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -20,43 +20,95 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN T
 
-CONFIGURATION="Release"
-VERBOSITY="quiet"
-XBUILD_ARGS="/verbosity:${VERBOSITY} /p:Configuration=${CONFIGURATION}"
-if [ "${OSTYPE}" = "linux-gnu" ]; then
-    XBUILD_ARGS="${XBUILD_ARGS} /p:TargetFrameworkVersion=v4.5"
-fi
+show_help () {
+    echo "build.sh: Build Yarn Spinner"
+    echo
+    echo "Usage: build.sh [bcdih]"
+    echo " -b: Build Yarn Spinner"
+    echo " -c: Clean any pre-existing builds and documentation"
+    echo " -d: Build documentation"
+    echo " -v: verbose output"
+    echo " -h: Show this text and exit"
+}
 
+init_build () {
+    CONFIGURATION="Release"
+    VERBOSITY="quiet"
+    while getopts "bcdhv" opt; do
+        case $opt in
+            c)
+                CLEAN=true ;;
+            d)
+                DOCUMENTATION=true ;;
+            v)
+                VERBOSITY=normal ;;
+            h)
+                HELP=true ;;
+        esac
+    XBUILD_ARGS="/verbosity:${VERBOSITY} /p:Configuration=${CONFIGURATION}"
+    if [ "${OSTYPE}" = "linux-gnu" ]; then
+        XBUILD_ARGS="${XBUILD_ARGS} /p:TargetFrameworkVersion=v4.5"
+    fi
+done
+}
+
+clean_yarnspinner () {
 if [ -f YarnSpinner/bin/Release/YarnSpinner.dll ]; then
     xbuild "${XBUILD_ARGS}" /target:clean YarnSpinner.sln
 fi
-xbuild "${XBUILD_ARGS}" YarnSpinner.sln
+}
 
-if [ $? -ne 0 ]; then
-    echo "Error during: xbuild ${XBUILD_ARGS}"
-    exit 1
-fi
+build_yarnspinner () {
+    xbuild "${XBUILD_ARGS}" YarnSpinner.sln
 
-# this is an appalling test for not windows or osx and with unity
-if [ "${OSTYPE}" != "linux-gnu" ]; then
-    OUTPUT_DLL="YarnSpinner.dll"
-    BUILD_DIR="YarnSpinner/bin/Release/"
-    UNITY_DIR="Unity/Assets/Yarn Spinner/Code/"
-
-    if [ -f "$BUILD_DIR/$OUTPUT_DLL" ]; then
-        cp -v "$BUILD_DIR/$OUTPUT_DLL" "$UNITY_DIR/$OUTPUT_DLL"
-    else
-        echo "Install for Unity failed."
+    if [ $? -ne 0 ]; then
+        echo "Error during: xbuild ${XBUILD_ARGS}"
         exit 1
     fi
+
+    # this is an appalling test for not windows or osx and with unity
+    if [ "${OSTYPE}" != "linux-gnu" ]; then
+        OUTPUT_DLL="YarnSpinner.dll"
+        BUILD_DIR="YarnSpinner/bin/Release/"
+        UNITY_DIR="Unity/Assets/Yarn Spinner/Code/"
+    
+        if [ -f "$BUILD_DIR/$OUTPUT_DLL" ]; then
+            cp -v "$BUILD_DIR/$OUTPUT_DLL" "$UNITY_DIR/$OUTPUT_DLL"
+        else
+            echo "Install for Unity failed."
+            exit 1
+        fi
+    fi
+}
+
+build_documentation () {
+    # Quick statement to build documents
+    if [ "$(which doxygen)" ]; then
+        if [ "$CLEAN" ]; then
+            echo "Cleaning documentation"
+            rm -fvr Documentation/{docbook,html,latex,rtf,xml}
+            rm -fvr GPATH GRTAGS GTAGS doxygen
+        fi
+        doxygen Documentation/Doxyfile
+    fi
+}
+
+# Pass all command line options to inuit_build
+init_build "$@"
+
+if [ "$HELP" ]; then
+    show_help
+    exit 1
 fi
-
-
-# Quick statement to build documents
-#if [ $(which doxygen) ]; then
-#    rm -fvr Documentation/{docbook,html,latex,rtf,xml}
-#    rm -fvr GPATH GRTAGS GTAGS doxygen
-#    doxygen Documentation/Doxyfile
-#
-#fi
-
+if [ "$CLEAN" ]; then
+    echo "Cleaning Yarn Spinner"
+    clean_yarnspinner
+fi
+echo "Building Yarn Spinner"
+if [ "$BUILD" ]; then
+    build_yarnspinner
+fi
+if [ "$DOCUMENTATION" ]; then
+    echo "Building documentation"
+    build_documentation
+fi
