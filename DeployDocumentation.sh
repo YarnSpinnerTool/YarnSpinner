175d2c888ca2423c7d6eb5d55d5cb7a20ef57ec5
Peter Lawler <relwalretep@gmail.com>
2017-05-30 12:11:56 +1000

    Remove extraneous files generated by Doxygen

--- DeployDocumentation.sh
+++ DeployDocumentation.sh
@@ -110,6 +110,10 @@ prepare_doxygen () {
         echo "Copying $l"
         cp --recursive $VERBOSE_SWITCH "$DOXYFILES_ROOT/$l" .
     done
+    # Remove un-needed files
+    rm -fvr "$DOXYFILES_ROOT/GPATH"
+    rm -fvr "$DOXYFILES_ROOT/GRTAGS"
+    rm -fvr "$DOXYFILES_ROOT/GTAGS"
 }
 
 commit_docs () {
