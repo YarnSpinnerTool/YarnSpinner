c3c12511d06d780e6bdd07423d55a9c2772a3caf
Schroedingers-Cat <6382400+Schroedingers-Cat@users.noreply.github.com>
2020-01-19 15:35:18 +0100

    Separate classes + more unity-like UX on yarn assets
    + Put runtime classes and editor classes into separate files
    + Editor class of a yarn asset inherits from ScriptedImporterEditor instead of Editor which means changes on the Asset will be applied by clicking either the "Apply" or "Revert" button making this more unity-like UX wise
    + Move Culture struct outside of YarnImporterEditor so it can be used in other classes too

--- Unity/Assets/YarnSpinner/Scripts/YarnProgram.cs
+++ Unity/Assets/YarnSpinner/Scripts/YarnProgram.cs
@@ -3,11 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using Yarn;
 
-#if UNITY_EDITOR
-using UnityEditor;
-using System.Linq;
-#endif
-
 /// Stores compiled Yarn programs in a form that Unity can serialise.
 public class YarnProgram : ScriptableObject
 {
@@ -24,44 +19,3 @@ public class YarnProgram : ScriptableObject
         return Program.Parser.ParseFrom(compiledProgram);                
     } 
 }
-
-#if UNITY_EDITOR
-/// A custom editor that lists the nodes in the program.
-[CustomEditor(typeof(YarnProgram))]
-public class YarnProgramEditor : Editor {
-
-    private List<string> nodeNames;
-
-    void OnEnable() {
-        try {
-            var program = (serializedObject.targetObject as YarnProgram).GetProgram();
-
-            nodeNames = program.Nodes.Keys.ToList();
-        } catch (YarnException) {
-            nodeNames = null;
-        }
-        
-    }
-
-    public override void OnInspectorGUI() {
-        base.OnInspectorGUI();
-
-        if (nodeNames == null) {
-            EditorGUILayout.HelpBox("Error reading Yarn program.", MessageType.Error);
-            return;
-        }
-        
-        EditorGUILayout.LabelField($"{nodeNames.Count} nodes:", EditorStyles.boldLabel);
-        EditorGUI.indentLevel += 1;
-
-        foreach (var nodeName in nodeNames) {
-            EditorGUILayout.LabelField(nodeName);
-        }
-
-        EditorGUI.indentLevel -= 1;
-        
-    }
-}
-#endif
-
-
