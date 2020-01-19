48eef2a8b33d6c8ed065b5f592d25942ca5ec26f
Schroedingers-Cat <6382400+Schroedingers-Cat@users.noreply.github.com>
2020-01-19 15:35:18 +0100

    Separate classes + more unity-like UX on yarn assets
    + Put runtime classes and editor classes into separate files
    + Editor class of a yarn asset inherits from ScriptedImporterEditor instead of Editor which means changes on the Asset will be applied by clicking either the "Apply" or "Revert" button making this more unity-like UX wise
    + Move Culture struct outside of YarnImporterEditor so it can be used in other classes too

---
+++ Unity/Assets/YarnSpinner/Editor/YarnProgramEditor.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+using System.Collections.Generic;
+using System.Linq;
+using Yarn;
+
+/// A custom editor that lists the nodes in the program.
+[CustomEditor(typeof(YarnProgram))]
+public class YarnProgramEditor : Editor {
+
+    private List<string> nodeNames;
+
+    void OnEnable() {
+        try {
+            var program = (serializedObject.targetObject as YarnProgram).GetProgram();
+
+            nodeNames = program.Nodes.Keys.ToList();
+        } catch (YarnException) {
+            nodeNames = null;
+        }
+
+    }
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+
+        if (nodeNames == null) {
+            EditorGUILayout.HelpBox("Error reading Yarn program.", MessageType.Error);
+            return;
+        }
+
+        EditorGUILayout.LabelField($"{nodeNames.Count} nodes:", EditorStyles.boldLabel);
+        EditorGUI.indentLevel += 1;
+
+        foreach (var nodeName in nodeNames) {
+            EditorGUILayout.LabelField(nodeName);
+        }
+
+        EditorGUI.indentLevel -= 1;
+    }
+}
---
+++ Unity/Assets/YarnSpinner/Scripts/Culture.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+[Serializable]
+public struct Culture {
+    public string Name;
+    public string DisplayName;
+}
