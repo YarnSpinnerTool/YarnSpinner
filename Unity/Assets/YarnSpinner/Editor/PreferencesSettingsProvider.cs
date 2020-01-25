9e83d924bfc34e00f11fb64040e5556c3fd96c4e
Schroedingers-Cat <6382400+Schroedingers-Cat@users.noreply.github.com>
2020-01-25 16:45:04 +0100

    LanguageSettings: Add Yarn entry to "Preferences" window
    + The window from "Edit->Preferences" now has an entry for yarn related user preferences
    + All classes, methods and variables have been renamed for improved consistency ("setting" is too broad, "preference" is being used from now on)
    * Preferences are automatically loaded from disk and stored to disk
    + Preferences are stored in the persistentDataPath because all the current preferences are machine-independent and should stay with the user's data

---
+++ Unity/Assets/YarnSpinner/Editor/PreferencesSettingsProvider.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+/// <summary>
+/// Yarn-related user preferences drawn in the "Preferences" window
+/// </summary>
+class PreferencesSettingsProvider : SettingsProvider {
+    public PreferencesSettingsProvider(string path, SettingsScope scope = SettingsScope.User) : base(path, scope) { }
+
+    private SerializedObject preferences;
+
+    public override void OnActivate(string searchContext, VisualElement rootElement) {
+        preferences = new SerializedObject(ScriptableObject.CreateInstance<Preferences>());
+    }
+
+    public override void OnDeactivate() {
+        if (preferences != null) {
+            Object.DestroyImmediate(preferences.targetObject);
+        }
+    }
+
+    public override void OnGUI(string searchContext) {
+        if (preferences == null || preferences.targetObject == null) {
+            return;
+        }
+
+        preferences.Update();
+
+        // Draw text language popup
+        var selectedTextLanguageIndex = -1;
+        var textLanguageProp = preferences.FindProperty("textLanguage");
+        var selectedTextLanguage = Cultures.AvailableCulturesNames
+            .Select((name, index) => new { name, index })
+            .FirstOrDefault(element => element.name == textLanguageProp.stringValue);
+        if (selectedTextLanguage != null) {
+            selectedTextLanguageIndex = selectedTextLanguage.index;
+        }
+        selectedTextLanguageIndex = EditorGUILayout.Popup("Text Language", selectedTextLanguageIndex, Cultures.AvailableCulturesDisplayNames);
+
+        textLanguageProp.stringValue = selectedTextLanguageIndex != -1
+            ? Cultures.AvailableCulturesNames[selectedTextLanguageIndex]
+            : System.Globalization.CultureInfo.CurrentCulture.Name;
+
+
+        // Draw audio language popup
+        var selectedAudioLanguageIndex = -1;
+        var audioLanguageProp = preferences.FindProperty("audioLanguage");
+        var selectedAudioLanguage = Cultures.AvailableCulturesNames
+            .Select((name, index) => new { name, index })
+            .FirstOrDefault(element => element.name == audioLanguageProp.stringValue);
+        if (selectedAudioLanguage != null) {
+            selectedAudioLanguageIndex = selectedAudioLanguage.index;
+        }
+        selectedAudioLanguageIndex = EditorGUILayout.Popup("Audio Language", selectedAudioLanguageIndex, Cultures.AvailableCulturesDisplayNames);
+
+        audioLanguageProp.stringValue = selectedAudioLanguageIndex != -1
+            ? Cultures.AvailableCulturesNames[selectedAudioLanguageIndex]
+            : System.Globalization.CultureInfo.CurrentCulture.Name;
+
+
+        preferences.ApplyModifiedProperties();
+    }
+
+    // Register the YarnSpinner user preferences in the "Preferences" window
+    [SettingsProvider]
+    public static SettingsProvider CreatePreferencesSettingsProvider() {
+        var provider = new PreferencesSettingsProvider("Preferences/Yarn Spinner", SettingsScope.User);
+
+        provider.keywords = new HashSet<string>(new[] { "Language", "Text", "Audio" });
+
+        return provider;
+    }
+}
---
+++ Unity/Assets/YarnSpinner/Scripts/Preferences.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.IO;
+using System.Globalization;
+
+[Serializable]
+public class Preferences : ScriptableObject {
+
+    public string textLanguage;
+    public string audioLanguage;
+
+    private string _preferencesPath;
+    private string textLanguageFromDisk;
+    private string audioLanguageFromDisk;
+
+    private void Awake() {
+        _preferencesPath = Application.persistentDataPath + "/preferences-language.json";
+        ReadPreferencesFromDisk();
+    }
+
+    private void OnEnable() {
+        if (string.IsNullOrEmpty(Cultures.AvailableCulturesNames.FirstOrDefault(element => element == textLanguage))) {
+            textLanguage = CultureInfo.CurrentCulture.Name;
+        }
+        if (string.IsNullOrEmpty(Cultures.AvailableCulturesNames.FirstOrDefault(element => element == audioLanguage))) {
+            audioLanguage = CultureInfo.CurrentCulture.Name;
+        }
+    }
+
+    private void OnDestroy() {
+        if (PreferencesChanged()) {
+            WritePreferencesToDisk();
+        }
+    }
+
+
+    public void ReadPreferencesFromDisk() {
+        // Check file's existence
+        bool fileExists = File.Exists(_preferencesPath);
+        if (!fileExists) {
+            // Wen don't throw an error since during OnEnable() all values will be initialized with 
+            // the system's default and create a new file once this class get's out of scope
+            Debug.Log("No previous Yarn Spinner preferences have been found.");
+            return;
+        }
+
+        // Load file into memory
+        string jsonString;
+        try {
+            jsonString = File.ReadAllText(_preferencesPath);
+        } catch (Exception) {
+            // No big deal since we'll initialize all values during OnEnable()
+            Debug.Log("Error loading Yarn Spinner preferences from JSON.");
+            return;
+        }
+
+        // Parse json to *this* ScriptableObject
+        try {
+            JsonUtility.FromJsonOverwrite(jsonString, this);
+        } catch (Exception) {
+            // No big deal since we'll initialize all values during OnEnable()
+            Debug.Log("Error parsing Yarn Spinner preferences from JSON.");
+            return;
+        }
+
+        // Apply text language preference from file
+        if (!string.IsNullOrEmpty(textLanguage)) {
+            var matchingTextLanguage = Cultures.AvailableCulturesNames.FirstOrDefault(element => element == textLanguage);
+            if (!string.IsNullOrEmpty(matchingTextLanguage)) {
+                // Language ID from JSON found in available Cultures so apply
+                textLanguage = matchingTextLanguage;
+            } else {
+                // Language ID from JSON was not found in available Cultures so reset
+                textLanguage = CultureInfo.CurrentCulture.Name;
+            }
+            textLanguageFromDisk = textLanguage;
+        }
+
+        // Apply audio language preference from file
+        if (!string.IsNullOrEmpty(audioLanguage)) {
+            var matchingAudioLanguage = Cultures.AvailableCulturesNames.FirstOrDefault(element => element == audioLanguage);
+            if (!string.IsNullOrEmpty(matchingAudioLanguage)) {
+                // Language ID from JSON found in available Cultures so apply
+                audioLanguage = matchingAudioLanguage;
+            } else {
+                // Language ID from JSON was not found in available Cultures so reset
+                audioLanguage = CultureInfo.CurrentCulture.Name;
+            }
+            audioLanguageFromDisk = audioLanguage;
+        }
+    }
+
+    public void WritePreferencesToDisk() {
+        string settingsJson = JsonUtility.ToJson(this, true);
+        try {
+            File.WriteAllText(_preferencesPath, settingsJson);
+        } catch (Exception) {
+            Debug.LogError("Saving Yarn Spinner preferences to disk failed!");
+        }
+    }
+
+    private bool PreferencesChanged() {
+        if (textLanguage != textLanguageFromDisk) {
+            return true;
+        }
+
+        if (audioLanguage != audioLanguageFromDisk) {
+            return true;
+        }
+
+        return false;
+    }
+}
