aca6ff14cc39111e12f83e592675232ef162b1a0
Schroedingers-Cat <6382400+Schroedingers-Cat@users.noreply.github.com>
2020-02-12 23:18:45 +0100

    Improve Inspector of yarn component used for general UI like main menus
    - Rename class "MainMenuYarnUI" to "YarnLinesAsCanvasText" to convey what the component can be used for
    - Display actual yarn line next to the referenced canvas text UI instead of "Element 0, 1, 2 ..."

---
+++ Unity/Assets/YarnSpinner/Editor/YarnLinesAsCanvasTextEditor.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Yarn.Unity {
+    [CustomEditor(typeof(YarnLinesAsCanvasText))]
+    public class YarnLinesAsCanvasTextEditor : Editor {
+        private YarnProgram _yarnProgram = default;
+        private SerializedProperty _yarnProgramProperty = default;
+        private Dictionary<string, string> _yarnLines = new Dictionary<string, string>();
+        private SerializedProperty _textObjectsProperty = default;
+        private bool ShowUIElements = true;
+        private const string textObjects = "Text Objects";
+        private string lastUpdateLanguage = default;
+
+        private void OnEnable() {
+            _yarnProgramProperty = serializedObject.FindProperty("yarnScript");
+            if (_yarnProgramProperty.objectReferenceValue == null) {
+                return;
+            }
+
+            _yarnProgram = _yarnProgramProperty.objectReferenceValue as YarnProgram;
+
+            _yarnLines.Clear();
+            foreach (var line in _yarnProgram.GetStringTable()) {
+                _yarnLines.Add(line.Key, line.Value);
+            }
+
+            _textObjectsProperty = serializedObject.FindProperty("textObjects");
+
+            lastUpdateLanguage = Preferences.TextLanguage;
+        }
+
+        public override void OnInspectorGUI() {
+            serializedObject.Update();
+
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(_yarnProgramProperty);
+            if (EditorGUI.EndChangeCheck() || lastUpdateLanguage != Preferences.TextLanguage) {
+                OnEnable();
+            }
+
+            if (_yarnProgramProperty.objectReferenceValue == null) {
+                EditorGUILayout.HelpBox("This component needs a yarn asset.", MessageType.Info);
+            } else {
+                var InventoryHeaderStyle = new GUIStyle();
+                InventoryHeaderStyle.fontStyle = FontStyle.Bold;
+
+                ShowUIElements = EditorGUILayout.Foldout(ShowUIElements, textObjects);
+                if (ShowUIElements) {
+                    if (_yarnLines.Count == 0) {
+                        EditorGUILayout.HelpBox("Couldn't find any text lines on the referenced Yarn asset.", MessageType.Info);
+                    } else {
+                        EditorGUILayout.BeginHorizontal();
+                        EditorGUILayout.LabelField("Yarn Text Lines", InventoryHeaderStyle);
+                        EditorGUILayout.LabelField("UI Canvas", InventoryHeaderStyle);
+                        EditorGUILayout.EndHorizontal();
+                        var i = 0;
+                        foreach (var stringEntry in _yarnLines) {
+                            if (_textObjectsProperty.arraySize <= i) {
+                                _textObjectsProperty.InsertArrayElementAtIndex(i);
+                            }
+                            GUIContent label = new GUIContent();
+                            label.text = "'" + stringEntry.Value + "'";
+                            EditorGUILayout.PropertyField(_textObjectsProperty.GetArrayElementAtIndex(i), label);
+                            i++;
+                        }
+                    }
+                }
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Unity/Assets/YarnSpinner/Scripts/YarnLinesAsCanvasText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Yarn.Unity {
+    public class YarnLinesAsCanvasText : MonoBehaviour {
+        public YarnProgram yarnScript;
+        private Dictionary<string, string> strings = new Dictionary<string, string>();
+        public UnityEngine.UI.Text[] textObjects;
+
+        private void Awake() {
+            foreach (var line in yarnScript.GetStringTable()) {
+                strings.Add(line.Key, line.Value);
+            }
+        }
+
+        // Start is called before the first frame update
+        void Start() {
+            var index = 0;
+            foreach (var line in strings) {
+                if (index >= textObjects.Length) {
+                    return;
+                }
+
+                if (textObjects[index]) {
+                    textObjects[index].text = line.Value;
+                }
+
+                index++;
+            }
+        }
+    }
+}
