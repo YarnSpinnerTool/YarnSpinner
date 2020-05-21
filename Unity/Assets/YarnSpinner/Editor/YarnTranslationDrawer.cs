1e27bf54c17d813d40c4dfb8da4ebbc146155a61
Schroedingers-Cat <6382400+Schroedingers-Cat@users.noreply.github.com>
2020-05-21 23:23:41 +0200

    YarnTranslationDrawer: Assign first localization language if expanding localization list via Size property

--- Unity/Assets/YarnSpinner/Editor/YarnTranslationDrawer.cs
+++ Unity/Assets/YarnSpinner/Editor/YarnTranslationDrawer.cs
@@ -1,11 +1,10 @@
-﻿using System.Globalization;
-using System.Linq;
+﻿using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
 [CustomPropertyDrawer(typeof(YarnTranslation))]
-public class YarnTranslationDrawer : PropertyDrawer 
-    {
+public class YarnTranslationDrawer : PropertyDrawer
+{
     /// <summary>
     /// The index of the language display name associated with this translation
     /// </summary>
@@ -15,14 +14,20 @@ public class YarnTranslationDrawer : PropertyDrawer
         label = EditorGUI.BeginProperty(position, label, property);
 
         var availableCulturesNames = Cultures.AvailableCulturesNames;
+        SerializedProperty languageNameProperty = property.FindPropertyRelative("languageName");
 
-        if (ProjectSettings.TextProjectLanguages.Count > 0 && ProjectSettings.TextProjectLanguages.Contains(property.FindPropertyRelative("languageName").stringValue)) {
-            // Only reduce text languages to selection from project settings if the current language is available in that list
+        // Only reduce text languages to selection from project settings if the current language is available in that list
+        if (ProjectSettings.TextProjectLanguages.Count > 0 && (ProjectSettings.TextProjectLanguages.Contains(languageNameProperty.stringValue) || string.IsNullOrEmpty(languageNameProperty.stringValue))) {
             availableCulturesNames = ProjectSettings.TextProjectLanguages.ToArray();
             // Check if this drawer is used on an asset utilizing a baseLanguageID and remove that from the available cultures to avoid double entries
             var baseLanguageIdProperty = property.serializedObject.FindProperty("baseLanguageID").stringValue;
             if (!string.IsNullOrEmpty(baseLanguageIdProperty)) {
                 availableCulturesNames = availableCulturesNames.Except(new string[] { baseLanguageIdProperty } ).ToArray();
+            }
+            // Assign the first localization language if no language has been selected (user has not clicked on "Create New Localisation" but expanded the localization list via the "Size" property)
+            if (string.IsNullOrEmpty(languageNameProperty.stringValue) && availableCulturesNames.Length > 0)
+            {
+                languageNameProperty.stringValue = availableCulturesNames[0];
             }
         }
 
@@ -30,10 +35,10 @@ public class YarnTranslationDrawer : PropertyDrawer
         Rect contentPosition = position;
         contentPosition.width *= 0.25f;
         EditorGUI.indentLevel = 1;
-        selectedLanguageIndex = EditorGUI.Popup(contentPosition, System.Array.IndexOf(availableCulturesNames, property.FindPropertyRelative("languageName").stringValue), Cultures.LanguageNamesToDisplayNames(availableCulturesNames));
+        selectedLanguageIndex = EditorGUI.Popup(contentPosition, System.Array.IndexOf(availableCulturesNames, languageNameProperty.stringValue), Cultures.LanguageNamesToDisplayNames(availableCulturesNames));
         // Apply changed language ID
         if (selectedLanguageIndex != -1) {
-            property.FindPropertyRelative("languageName").stringValue = availableCulturesNames[selectedLanguageIndex];
+            languageNameProperty.stringValue = availableCulturesNames[selectedLanguageIndex];
         }
 
         // The yarn translation file
