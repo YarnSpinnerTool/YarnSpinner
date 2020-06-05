427a093e0d3340b65e81f32a8b2de83775f98958
Jon Manning <jon@secretlab.com.au>
2020-06-05 14:12:24 +1000

    Remove YarnSpinnerConsole A replacement for it is available at: https://github.com/YarnSpinnerTool/YarnSpinner-Console/

--- YarnSpinnerConsole/GlobalSuppressions.cs
+++
@@ -1,7 +0,0 @@
-﻿
-// This file is used by Code Analysis to maintain SuppressMessage 
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2208:Instantiate argument exceptions correctly", Justification = "This SHOULD be fixed (to have more useful error messages), but it's out of scope for fixing locale issues.")]
