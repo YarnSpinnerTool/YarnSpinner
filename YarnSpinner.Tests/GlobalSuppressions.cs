9d8c0b1ba265683ccb1e5abf351d4481034980c4
Jon Manning <jon@secretlab.com.au>
2020-07-03 14:21:52 +1000

    Don't suppress warning CA1016 (no longer needed)

--- YarnSpinner.Tests/GlobalSuppressions.cs
+++ YarnSpinner.Tests/GlobalSuppressions.cs
@@ -4,5 +4,4 @@
 // Project-level suppressions either have no target or are given 
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1016:Mark assemblies with assembly version", Justification = "You don't really NEED this, I suppose.")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1051:Do not declare visible instance fields", Justification = "These normally would be properties.")]
