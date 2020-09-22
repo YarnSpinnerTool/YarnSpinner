3079aac3087ea27898d02bc10902d0c764c381cf
Jon Manning <jon@secretlab.com.au>
2020-09-22 22:05:46 +1000

    Fix parse errors not including useful messages

--- YarnSpinner.Compiler/ErrorListener.cs
+++ YarnSpinner.Compiler/ErrorListener.cs
@@ -73,7 +73,7 @@
                 }
             }
 
-            throw new ParseException(builder.ToString());
+            throw new ParseException(null, builder.ToString(), null);
         }
     }
 }
