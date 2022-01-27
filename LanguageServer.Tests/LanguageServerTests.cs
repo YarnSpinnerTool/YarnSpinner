b96092c26f59a1ac3f62d4e9a0829f99b35874c5
Jon Manning <jon@secretlab.com.au>
2022-01-27 14:33:19 +1100

    Refactor out test base class

--- LanguageServer.Tests/LanguageServerTests.cs
+++ LanguageServer.Tests/LanguageServerTests.cs
@@ -1,26 +1,12 @@
-using System;
 using System.Threading.Tasks;
 using FluentAssertions;
-using OmniSharp.Extensions.JsonRpc.Server;
-using OmniSharp.Extensions.JsonRpc.Testing;
-using OmniSharp.Extensions.LanguageProtocol.Testing;
-using OmniSharp.Extensions.LanguageServer.Client;
-using OmniSharp.Extensions.LanguageServer.Server;
 using OmniSharp.Extensions.LanguageServer.Protocol.Document;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
-using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
-using OmniSharp.Extensions.LanguageServer.Protocol.Workspace;
-using OmniSharp.Extensions.LanguageServer.Protocol.Serialization;
 using Xunit;
 using Xunit.Abstractions;
-using OmniSharp.Extensions.LanguageServer.Protocol.Document;
 using System.Linq;
-using System.Collections.Generic;
 
 using System.IO;
-
-using Range = OmniSharp.Extensions.LanguageServer.Protocol.Models.Range;
-using OmniSharp.Extensions.LanguageServer.Protocol.Client;
 using FluentAssertions.Execution;
 
 #pragma warning disable CS0162
@@ -29,49 +15,10 @@ using FluentAssertions.Execution;
 
 namespace YarnLanguageServer.Tests
 {
-    public class YarnLanguageServerTests : LanguageProtocolTestBase
+    public class LanguageServerTests : LanguageServerTestsBase
     {
-        public YarnLanguageServerTests(ITestOutputHelper outputHelper) : base(
-            new JsonRpcTestOptions()
-        //.ConfigureForXUnit(outputHelper)
-        )
+        public LanguageServerTests(ITestOutputHelper outputHelper) : base(outputHelper)
         {
-        }
-
-        TaskCompletionSource<List<Diagnostic>> ReceivedDiagnosticsNotification = new();
-
-        /// <summary>
-        /// Waits for diagnostics to be returned (via <see
-        /// cref="ReceivedDiagnosticsNotification"/> being completed), and
-        /// returns those diagnostics. If diagnostics are not returned before
-        /// the specified timeout elapses, an exception is thrown.
-        /// </summary>
-        /// <param name="timeout">The amount of time to wait for
-        /// diagnostics.</param>
-        /// <returns>A collection of <see cref="Diagnostic"/> objects.</returns>
-        private async Task<IEnumerable<Diagnostic>> GetDiagnosticsAsync(double timeout = 2f)
-        {
-            try
-            {
-                // Timeout.
-                var winner = await Task.WhenAny(
-                    ReceivedDiagnosticsNotification.Task,
-                    Task.Delay(
-                        TimeSpan.FromSeconds(timeout),
-                        CancellationToken
-                    )
-                );
-                ReceivedDiagnosticsNotification.Task.Should().BeSameAs(winner);
-
-                Assert.Same(ReceivedDiagnosticsNotification.Task, winner);
-
-                return await ReceivedDiagnosticsNotification.Task;
-            }
-            finally
-            {
-                // Get ready for the next call
-                ReceivedDiagnosticsNotification = new();
-            }
         }
 
         [Fact]
@@ -81,37 +28,6 @@ namespace YarnLanguageServer.Tests
 
             client.ServerSettings.Should().NotBeNull();
             client.ClientSettings.Should().NotBeNull();
-        }
-
-        [Fact]
-        public async Task Server_OnInvalidChanges_ProducesSyntaxErrors()
-        {
-            var (client, server) = await Initialize(ConfigureClient, ConfigureServer);
-
-            {
-                var errors = (await GetDiagnosticsAsync()).Where(d => d.Severity == DiagnosticSeverity.Error);
-
-                errors.Should().BeNullOrEmpty("because the original document contains no syntax errors");
-            }
-
-            // Introduce an error
-            var filePath = Path.Combine(PathToTestData, "Test.yarn");
-            ChangeTextInDocument(client, filePath, new Position(8, 0), "<<set");
-
-            {
-                var errors = (await GetDiagnosticsAsync()).Where(d => d.Severity == DiagnosticSeverity.Error);
-
-                errors.Should().NotBeNullOrEmpty("because we have introduced a syntax error");
-            }
-
-            // Remove the error
-            ChangeTextInDocument(client, filePath, new Position(8, 0), new Position(8, 5), "");
-
-            {
-                var errors = (await GetDiagnosticsAsync()).Where(d => d.Severity == DiagnosticSeverity.Error);
-
-                errors.Should().BeNullOrEmpty("because the syntax error was removed");
-            }
         }
 
         [Fact]
@@ -153,6 +69,37 @@ namespace YarnLanguageServer.Tests
 
                 completions.Should().NotContain(c => c.Label == "Start",
                     "because 'Start' is a node name, and not a command");
+            }
+        }
+
+        [Fact]
+        public async Task Server_OnInvalidChanges_ProducesSyntaxErrors()
+        {
+            var (client, server) = await Initialize(ConfigureClient, ConfigureServer);
+
+            {
+                var errors = (await GetDiagnosticsAsync()).Where(d => d.Severity == DiagnosticSeverity.Error);
+
+                errors.Should().BeNullOrEmpty("because the original document contains no syntax errors");
+            }
+
+            // Introduce an error
+            var filePath = Path.Combine(PathToTestData, "Test.yarn");
+            ChangeTextInDocument(client, filePath, new Position(8, 0), "<<set");
+
+            {
+                var errors = (await GetDiagnosticsAsync()).Where(d => d.Severity == DiagnosticSeverity.Error);
+
+                errors.Should().NotBeNullOrEmpty("because we have introduced a syntax error");
+            }
+
+            // Remove the error
+            ChangeTextInDocument(client, filePath, new Position(8, 0), new Position(8, 5), "");
+
+            {
+                var errors = (await GetDiagnosticsAsync()).Where(d => d.Severity == DiagnosticSeverity.Error);
+
+                errors.Should().BeNullOrEmpty("because the syntax error was removed");
             }
         }
 
@@ -204,162 +151,6 @@ namespace YarnLanguageServer.Tests
                 completions.Should().NotContain(c => c.Label == "Node2",
                     "because while 'Node2' is a node we could jump to, we're currently in a syntax error");
             }
-        }
-
-        private static void ChangeTextInDocument(ILanguageClient client, string fileURI, Position start, string text)
-        {
-            ChangeTextInDocument(client, fileURI, start, start, text);
-        }
-
-        private static void ChangeTextInDocument(ILanguageClient client, string fileURI, Position start, Position end, string text)
-        {
-            client.DidChangeTextDocument(new DidChangeTextDocumentParams
-            {
-                TextDocument = new OptionalVersionedTextDocumentIdentifier
-                {
-                    Uri = fileURI,
-                },
-                ContentChanges = new[] {
-                    new TextDocumentContentChangeEvent {
-                        Range = new Range {
-                            Start = start,
-                            End = end
-                        },
-                        Text = text,
-                    },
-                }
-            });
-        }
-
-        // [Fact]
-        // public async Task Server_Should_Stay_Alive_When_Requests_Throw_An_Exception()
-        // {
-        //     var (client, _) = await Initialize(ConfigureClient, ConfigureServer);
-
-        //     var result = await client.SendRequest("keepalive").Returning<bool>(CancellationToken);
-        //     result.Should().BeTrue();
-
-        //     Func<Task> a = () => client.SendRequest("throw").ReturningVoid(CancellationToken);
-        //     await a.Should().ThrowAsync<InternalErrorException>();
-
-        //     result = await client.SendRequest("keepalive").Returning<bool>(CancellationToken);
-        //     result.Should().BeTrue();
-        // }
-
-        // [Fact]
-        // public async Task Client_Should_Stay_Alive_When_Requests_Throw_An_Exception()
-        // {
-        //     var (_, server) = await Initialize(ConfigureClient, ConfigureServer);
-
-        //     var result = await server.SendRequest("keepalive").Returning<bool>(CancellationToken);
-        //     result.Should().BeTrue();
-
-        //     Func<Task> a = () => server.SendRequest("throw").ReturningVoid(CancellationToken);
-        //     await a.Should().ThrowAsync<InternalErrorException>();
-
-        //     result = await server.SendRequest("keepalive").Returning<bool>(CancellationToken);
-        //     result.Should().BeTrue();
-        // }
-
-        // [Fact]
-        // public async Task Server_Should_Support_Links()
-        // {
-        //     var (client, _) = await Initialize(ConfigureClient, ConfigureServer);
-
-        //     var result = await client.SendRequest("ka").Returning<bool>(CancellationToken);
-        //     result.Should().BeTrue();
-
-        //     Func<Task> a = () => client.SendRequest("t").ReturningVoid(CancellationToken);
-        //     await a.Should().ThrowAsync<InternalErrorException>();
-
-        //     result = await client.SendRequest("ka").Returning<bool>(CancellationToken);
-        //     result.Should().BeTrue();
-        // }
-
-        // [Fact]
-        // public async Task Client_Should_Support_Links()
-        // {
-        //     var (_, server) = await Initialize(ConfigureClient, ConfigureServer);
-
-        //     var result = await server.SendRequest("ka").Returning<bool>(CancellationToken);
-        //     result.Should().BeTrue();
-
-        //     Func<Task> a = () => server.SendRequest("t").ReturningVoid(CancellationToken);
-        //     await a.Should().ThrowAsync<InternalErrorException>();
-
-        //     result = await server.SendRequest("ka").Returning<bool>(CancellationToken);
-        //     result.Should().BeTrue();
-        // }
-
-        private void ConfigureClient(LanguageClientOptions options)
-        {
-            options.OnRequest("keepalive", ct => Task.FromResult(true));
-            options.WithLink("keepalive", "ka");
-            options.WithLink("throw", "t");
-            options.OnRequest(
-                "throw", async ct =>
-                {
-                    throw new NotSupportedException();
-                    return Task.CompletedTask;
-                }
-            );
-
-            options.OnPublishDiagnostics((diagnosticsParams) =>
-            {
-                var diagnostics = diagnosticsParams.Diagnostics.ToList();
-                ReceivedDiagnosticsNotification.TrySetResult(diagnostics);
-            });
-
-            options.WithRootPath(PathToTestData);
-        }
-
-        private static string PathToTestData
-        {
-            get
-            {
-                var context = AppContext.BaseDirectory;
-
-                var directoryContainingProject = GetParentDirectoryContainingFile(new DirectoryInfo(context), "*.csproj");
-
-                if (directoryContainingProject != null)
-                {
-                    return System.IO.Path.Combine(directoryContainingProject.FullName, "TestData");
-                }
-                else
-                {
-                    throw new InvalidOperationException("Failed to find path containing .csproj!");
-                }
-
-                static DirectoryInfo? GetParentDirectoryContainingFile(DirectoryInfo directory, string filePattern)
-                {
-                    var current = directory;
-                    do
-                    {
-                        if (current.EnumerateFiles(filePattern).Any())
-                        {
-                            return current;
-                        }
-                        current = current.Parent;
-                    } while (current != null);
-
-                    return null;
-                }
-            }
-        }
-
-        private void ConfigureServer(LanguageServerOptions options)
-        {
-            // options.OnRequest("keepalive", ct => Task.FromResult(true));
-            // options.WithLink("keepalive", "ka");
-            // options.WithLink("throw", "t");
-            // options.OnRequest(
-            //     "throw", async ct => {
-            //         throw new NotSupportedException();
-            //         return Task.CompletedTask;
-            //     }
-            // );
-
-            YarnLanguageServer.ConfigureOptions(options);
         }
     }
 }
---
+++ LanguageServer.Tests/LanguageServerTestsBase.cs
@@ -0,0 +1,158 @@
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using OmniSharp.Extensions.JsonRpc.Testing;
+using OmniSharp.Extensions.LanguageProtocol.Testing;
+using OmniSharp.Extensions.LanguageServer.Client;
+using OmniSharp.Extensions.LanguageServer.Server;
+using OmniSharp.Extensions.LanguageServer.Protocol.Document;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using Xunit;
+using Xunit.Abstractions;
+using System.Linq;
+using System.Collections.Generic;
+
+using System.IO;
+
+using Range = OmniSharp.Extensions.LanguageServer.Protocol.Models.Range;
+using OmniSharp.Extensions.LanguageServer.Protocol.Client;
+using FluentAssertions.Execution;
+
+#pragma warning disable CS0162
+
+#pragma warning disable VSTHRD200 // async method names should end with "Async"
+
+namespace YarnLanguageServer.Tests
+{
+    public class LanguageServerTestsBase : LanguageProtocolTestBase
+    {
+        public LanguageServerTestsBase(ITestOutputHelper outputHelper) : base(
+            new JsonRpcTestOptions()
+        //.ConfigureForXUnit(outputHelper)
+        )
+        {
+        }
+
+        TaskCompletionSource<List<Diagnostic>> ReceivedDiagnosticsNotification = new();
+
+        protected static string PathToTestData
+        {
+            get
+            {
+                var context = AppContext.BaseDirectory;
+
+                var directoryContainingProject = GetParentDirectoryContainingFile(new DirectoryInfo(context), "*.csproj");
+
+                if (directoryContainingProject != null)
+                {
+                    return System.IO.Path.Combine(directoryContainingProject.FullName, "TestData");
+                }
+                else
+                {
+                    throw new InvalidOperationException("Failed to find path containing .csproj!");
+                }
+
+                static DirectoryInfo? GetParentDirectoryContainingFile(DirectoryInfo directory, string filePattern)
+                {
+                    var current = directory;
+                    do
+                    {
+                        if (current.EnumerateFiles(filePattern).Any())
+                        {
+                            return current;
+                        }
+                        current = current.Parent;
+                    } while (current != null);
+
+                    return null;
+                }
+            }
+        }
+
+        protected static void ChangeTextInDocument(ILanguageClient client, string fileURI, Position start, string text)
+        {
+            ChangeTextInDocument(client, fileURI, start, start, text);
+        }
+
+        protected static void ChangeTextInDocument(ILanguageClient client, string fileURI, Position start, Position end, string text)
+        {
+            client.DidChangeTextDocument(new DidChangeTextDocumentParams
+            {
+                TextDocument = new OptionalVersionedTextDocumentIdentifier
+                {
+                    Uri = fileURI,
+                },
+                ContentChanges = new[] {
+                    new TextDocumentContentChangeEvent {
+                        Range = new Range {
+                            Start = start,
+                            End = end
+                        },
+                        Text = text,
+                    },
+                }
+            });
+        }
+
+        protected void ConfigureClient(LanguageClientOptions options)
+        {
+            options.OnRequest("keepalive", ct => Task.FromResult(true));
+            options.WithLink("keepalive", "ka");
+            options.WithLink("throw", "t");
+            options.OnRequest(
+                "throw", async ct =>
+                {
+                    throw new NotSupportedException();
+                    return Task.CompletedTask;
+                }
+            );
+
+            options.OnPublishDiagnostics((diagnosticsParams) =>
+            {
+                var diagnostics = diagnosticsParams.Diagnostics.ToList();
+                ReceivedDiagnosticsNotification.TrySetResult(diagnostics);
+            });
+
+            options.WithRootPath(PathToTestData);
+        }
+
+        protected void ConfigureServer(LanguageServerOptions options)
+        {
+            YarnLanguageServer.ConfigureOptions(options);
+        }
+
+        /// <summary>
+        /// Waits for diagnostics to be returned (via <see
+        /// cref="ReceivedDiagnosticsNotification"/> being completed), and
+        /// returns those diagnostics. If diagnostics are not returned before
+        /// the specified timeout elapses, an exception is thrown.
+        /// </summary>
+        /// <param name="timeout">The amount of time to wait for
+        /// diagnostics.</param>
+        /// <returns>A collection of <see cref="Diagnostic"/> objects.</returns>
+        protected async Task<IEnumerable<Diagnostic>> GetDiagnosticsAsync(double timeout = 2f)
+        {
+            try
+            {
+                // Timeout.
+                var winner = await Task.WhenAny(
+                    ReceivedDiagnosticsNotification.Task,
+                    Task.Delay(
+                        TimeSpan.FromSeconds(timeout),
+                        CancellationToken
+                    )
+                );
+                ReceivedDiagnosticsNotification.Task.Should().BeSameAs(winner);
+
+                Assert.Same(ReceivedDiagnosticsNotification.Task, winner);
+
+                return await ReceivedDiagnosticsNotification.Task;
+            }
+            finally
+            {
+                // Get ready for the next call
+                ReceivedDiagnosticsNotification = new();
+            }
+        }
+    }
+}
