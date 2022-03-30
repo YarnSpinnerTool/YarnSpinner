2242a5f9e121282e56a0a63010eb545b5d770e89
Tim Nugent <tim@lonely.coffee>
2022-03-30 15:39:23 +1100

    added more tag tests

--- YarnSpinner.Tests/TagTests.cs
+++ YarnSpinner.Tests/TagTests.cs
@@ -10,6 +10,19 @@ namespace YarnSpinner.Tests
         }
 
         [Fact]
+        void TestNoOptionsLineNotTagged()
+        {
+            var source = "title:Start\n---\nline without options #line:1\n===\n";
+
+            var result = Compiler.Compile(CompilationJob.CreateFromString("input", source));
+            Assert.Empty(result.Diagnostics);
+
+            var info = result.StringTable["line:1"];
+
+            Assert.DoesNotContain("lastline",info.metadata);
+        }
+
+        [Fact]
         void TestLineBeforeOptionsTaggedLastLine()
         {
             var source = "title:Start\n---\nline before options #line:1\n-> option 1\n-> option 2\n===\n";
@@ -86,6 +99,69 @@ line before options #line:0
             Assert.Empty(result.Diagnostics);
             var info = result.StringTable["line:0"];
             Assert.Contains("lastline", info.metadata);
+        }
+        [Fact]
+        void TestIfInteriorLinesNotTaggedLastLine()
+        {
+            var source = CreateTestNode(@"
+<<if true>>
+line before options #line:0
+<<endif>>
+-> option 1
+-> option 2
+            ");
+
+            var result = Compiler.Compile(CompilationJob.CreateFromString("input", source));
+            Assert.Empty(result.Diagnostics);
+            var info = result.StringTable["line:0"];
+            Assert.DoesNotContain("lastline", info.metadata);
+        }
+
+        [Fact]
+        void TestNestedOptionLinesNotTagged()
+        {
+            var source = CreateTestNode(@"
+-> option 1
+    inside options #line:1a
+-> option 2
+-> option 3
+            ");
+
+            var result = Compiler.Compile(CompilationJob.CreateFromString("input", source));
+            Assert.Empty(result.Diagnostics);
+
+            var info = result.StringTable["line:1a"];
+            Assert.DoesNotContain("lastline", info.metadata);
+        }
+
+        [Fact]
+        void TestInterruptedLinesNotTagged()
+        {
+            var source = CreateTestNode(@"
+line before command #line:0
+<<custom command>>
+-> option 1
+line before declare #line:1
+<<declare $value = 0>>
+-> option 1
+line before set #line:2
+<<set $value = 0>>
+-> option 1
+line before jump #line:3
+<<jump nodename>>
+            ");
+
+            var result = Compiler.Compile(CompilationJob.CreateFromString("input", source));
+            Assert.Empty(result.Diagnostics);
+
+            var info = result.StringTable["line:0"];
+            Assert.DoesNotContain("lastline", info.metadata);
+            info = result.StringTable["line:1"];
+            Assert.DoesNotContain("lastline", info.metadata);
+            info = result.StringTable["line:2"];
+            Assert.DoesNotContain("lastline", info.metadata);
+            info = result.StringTable["line:3"];
+            Assert.DoesNotContain("lastline", info.metadata);
         }
     }
 }
