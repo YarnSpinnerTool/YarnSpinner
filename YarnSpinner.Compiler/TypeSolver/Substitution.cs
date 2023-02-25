e9a35cdbb19786361b8abfcd953d1dd313c69c6d
Jon Manning <jon@secretlab.com.au>
2023-02-25 18:56:59 +1100

    Add Substitution.TryResolveTypeVariable This is a utility method that attempts to resolve a single TypeVariable, used in the unit tests.

--- YarnSpinner.Compiler/TypeSolver/Substitution.cs
+++ YarnSpinner.Compiler/TypeSolver/Substitution.cs
@@ -27,6 +27,30 @@ namespace TypeChecker
             clone.FailureChain = new List<string>(FailureChain);
             return clone;
         }
+
+        public bool TryResolveTypeVariable(TypeVariable variable, out IType result) {
+            var checkedSet = new HashSet<TypeVariable>();
+
+            var current = variable;
+
+            while (checkedSet.Contains(current) == false) {
+                if (this.ContainsKey(current) == false) {
+                    result = default;
+                    return false;
+                }
+                result = this[current];
+                if (result is TypeVariable typeVariable) {
+                    checkedSet.Add(current);
+                    current = typeVariable;
+                    continue;
+                } else {
+                    return true;
+                }
+            }
+
+            result = default;
+            return false;
+        }
     }
 
 }
