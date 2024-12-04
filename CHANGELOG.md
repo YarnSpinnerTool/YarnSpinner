b11586339c44f91bdbeba2b3280d4498d30f58ae
Jon Manning <jon@secretlab.com.au>
2024-12-04 15:57:30 +1100

    Emit warnings for additional text on the same line as a statement

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 ### Added
 
+- The compiler will now warn if additional text is present on the same line before or after commands and other statements.
+  - For example, the following code will emit a warning, because the last `>` character is likely a typo: `<<wait 5>>>`
+
 ### Changed
 
 ### Removed
--- YarnSpinner.Compiler/Visitors/StyleWarningsVisitor.cs
+++ YarnSpinner.Compiler/Visitors/StyleWarningsVisitor.cs
@@ -19,5 +19,47 @@ namespace Yarn.Compiler
         {
             this.tokenStream = tokenStream;
         }
+
+        public override int VisitStatement([NotNull] YarnSpinnerParser.StatementContext context)
+        {
+            List<IToken>? tokensOnLine = null;
+
+            // If the first token is a COMMAND_START, then it should be the
+            // first token on the channel for that line.
+            if (context.Start.Type == YarnSpinnerLexer.COMMAND_START)
+            {
+                tokensOnLine ??= GetAllTokensOnLine(tokenStream, context.Start.Line);
+
+                var indexOfStartToken = tokensOnLine.IndexOf(context.Start);
+
+                if (indexOfStartToken != 0)
+                {
+                    AddDiagnostic(new Diagnostic(this.FileName, tokensOnLine[indexOfStartToken - 1], "Commands should start on a new line", Diagnostic.DiagnosticSeverity.Warning));
+                }
+            }
+
+            // If the last token is a COMMAND_END, then it should be the last
+            // token on the channel for that line.
+            if (context.Stop.Type == YarnSpinnerLexer.COMMAND_END)
+            {
+                tokensOnLine ??= GetAllTokensOnLine(tokenStream, context.Stop.Line);
+
+                var indexOfStartToken = tokensOnLine.IndexOf(context.Stop);
+
+                if (indexOfStartToken != (tokensOnLine.Count - 1))
+                {
+                    AddDiagnostic(new Diagnostic(this.FileName, tokensOnLine[indexOfStartToken + 1], "Commands should have no text after them", Diagnostic.DiagnosticSeverity.Warning));
+                }
+            }
+
+            return base.VisitStatement(context);
+        }
+
+        public static List<IToken> GetAllTokensOnLine(CommonTokenStream tokenStream, int line, int channel = Lexer.DefaultTokenChannel)
+        {
+            return tokenStream.GetTokens()
+                .Where(t => t.Channel == channel && t.Line == line)
+                .ToList();
+        }
     }
 }
