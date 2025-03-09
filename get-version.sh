fa087ce2f4db3e8b0c0974ce0061b11b63b8564e
Jon Manning <jon@secretlab.com.au>
2025-03-09 13:56:48 +1100

    Allow overriding version in get-version.sh

--- get-version.sh
+++ get-version.sh
@@ -11,25 +11,28 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 # [prerelease]
 REGEX='v(\d+)\.(\d+)\.(\d+)(-.*)?'
 
-# Extract the data from the version number
-major=$(echo $most_recent_tag | perl -pe "s|$REGEX|\1|" )
-minor=$(echo $most_recent_tag | perl -pe "s|$REGEX|\2|" )
-patch=$(echo $most_recent_tag | perl -pe "s|$REGEX|\3|" )
-prerelease=$(echo $most_recent_tag | perl -pe "s|$REGEX|\4|" )
+raw_version=${1:-"$most_recent_tag"}
 
-# Create the version strings we'll write into the AssemblyInfo files
-OutputAssemblyVersion="$major.$minor.$patch.$commits_since_tag"
-OutputAssemblyInformationalVersion="$major.$minor.$patch$prerelease+$commits_since_tag.Branch.$branch.Sha.$sha"
-OutputAssemblyFileVersion="$major.$minor.$patch.$commits_since_tag"
+# Extract the data from the version number
+major=$(echo $raw_version | perl -pe "s|$REGEX|\1|" )
+minor=$(echo $raw_version | perl -pe "s|$REGEX|\2|" )
+patch=$(echo $raw_version | perl -pe "s|$REGEX|\3|" )
+prerelease=$(echo $raw_version | perl -pe "s|$REGEX|\4|" )
 
 # Calculate the semver from the version (should be the same as the version, but
 # just in case)
 SemVer="$major.$minor.$patch$prerelease"
 
-# If there are any commits since the current tag, add that note
-if [ "$commits_since_tag" -gt 0 ]; then
+# If there are any commits since the current tag and we aren't overriding our
+# version, add that note
+if [ "$commits_since_tag" -gt 0 -a -z "$1"  ]; then
     SemVer="$SemVer+$commits_since_tag"
 fi
+
+# Create the version strings we'll write into the AssemblyInfo files
+OutputAssemblyVersion=$(echo "$major.$minor.$patch.$commits_since_tag" | perl -pe "s|\/|\\\/|" )
+OutputAssemblyInformationalVersion=$(echo "$SemVer.Branch.$branch.Sha.$sha" | perl -pe "s|\/|\\\/|" )
+OutputAssemblyFileVersion=$(echo "$major.$minor.$patch.$commits_since_tag" | perl -pe "s|\/|\\\/|" )
 
 # Update the AssemblyInfo.cs files
 for infoFile in $(find . -name "AssemblyInfo.cs"); do
