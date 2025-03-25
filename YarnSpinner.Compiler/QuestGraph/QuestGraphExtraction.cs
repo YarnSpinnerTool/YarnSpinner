521ba3a9bf01ae1ca1c68f658fce1d23bb24ea19
Jon Manning <jon@secretlab.com.au>
2025-03-25 16:55:48 +1100

    Generate rules for graph edge descriptors

--- YarnSpinner.Compiler/QuestGraph/QuestGraphExtraction.cs
+++ YarnSpinner.Compiler/QuestGraph/QuestGraphExtraction.cs
@@ -16,6 +16,12 @@ namespace Yarn.Compiler
             return visitor.Edges;
         }
 
+        public static string? ExtractQuestGraphAsJson(Project project, string existingQuestGraph)
+        {
+            var existingQuestGraphDocument = JsonDocument.Parse(existingQuestGraph);
+            return System.Text.Json.JsonSerializer.Serialize(ExtractQuestGraph(project, existingQuestGraphDocument));
+        }
+
         public static System.Text.Json.JsonDocument? ExtractQuestGraph(Project project, JsonDocument? existingQuestGraph)
         {
 
@@ -55,6 +61,7 @@ namespace Yarn.Compiler
             var goalsJSON = GetOrCreateArray("goals");
             var variablesJSON = GetOrCreateArray("variables");
             var edgesJSON = GetOrCreateArray("edges");
+            var rulesJSON = GetOrCreateArray("rules");
 
             static JsonObject GetOrCreateElement(JsonArray array, System.Func<JsonNode, bool> predicate)
             {
@@ -129,6 +136,40 @@ namespace Yarn.Compiler
                     variableJSON["source"] = edgeData.VariableCreation == QuestGraphEdge.VariableType.Implicit ? "CreatedInEditor" : "ImportedFromYarnScript";
 
                     SetValueIfNotExists(variableJSON, "description", edgeData.Description ?? "(no description)");
+
+                    if (edgeData.VariableCreation == QuestGraphEdge.VariableType.Implicit)
+                    {
+                        // Create a rule that this variable is only true if its
+                        // source node is reachable (i.e. the only way that it
+                        // becomes true is when it completes the parent)
+                        var ruleJSON = GetOrCreateElement(rulesJSON, r =>
+                        {
+                            var condition = r["condition"]?.AsObject();
+                            if (condition == null) { return false; }
+                            var implies = condition["implies"]?.AsArray();
+                            if (implies == null) { return false; }
+
+                            var variable = implies[0]?.GetValue<string>();
+
+                            var nodeConstraintObj = implies[1]?.AsObject()["node"];
+                            var nodeConstraint = nodeConstraintObj?.GetValue<string>();
+
+
+                            // return false;
+                            return variable == edgeData.VariableName && nodeConstraint == edgeData.FromNode.FullName;
+                        });
+
+                        ruleJSON["name"] = ruleJSON["name"] ?? edgeData.FromNode.FullName;
+
+                        ruleJSON["condition"] = JsonObject.Parse(@$"{{""implies"":[
+                            ""{edgeData.VariableName}"",
+                            {{
+                                ""node"":""{edgeData.FromNode.FullName}"",
+                                ""state"": ""Reachable""
+                            }}
+                        ]}}");
+
+                    }
                 }
             }
 
